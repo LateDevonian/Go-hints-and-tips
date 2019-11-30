d5ce7513fdf9e6813e9ba3020cb1ffc55102adbd
LateDevonian <agean27@gmail.com>
2019-12-01 09:07:27 +1100

    udp logging

---
+++ chapter5_debug_test/24_back_pressure/udp_logging.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net"
+	"time"
+)
+
+//run nc -lk 1902 to satart a server using netcat
+
+func main() {
+	timeout := 30 * time.Second
+	conn, err := net.DialTimeout("udp", "localhost:1902", timeout)
+	if err != nil {
+		panic("Failed to connect to localhost:1902")
+	}
+	defer conn.Close()
+
+	f := log.Ldate | log.Lshortfile
+	logger := log.New(conn, "example ", f)
+
+	logger.Println("this is a regular message.")
+	logger.Panicln("This is a panic")
+}

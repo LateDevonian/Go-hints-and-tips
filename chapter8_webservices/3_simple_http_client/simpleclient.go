a42f926709a6018bb6419e6fbb794e6093db72d5
LateDevonian <agean27@gmail.com>
2019-12-09 19:54:15 +1100

    simpleclient

--- chapter8_webservices/3_simple_http_client/simpleclient.go
+++ chapter8_webservices/3_simple_http_client/simpleclient.go
@@ -1,5 +1,23 @@
 package main
 
-func min() {
-	cc
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	//create custom http climeout with timeout of 1 second
+	cc := &http.Client{Timeout: time.Second}
+	//handles timeout
+	res, err := cc.Get("http://goinpracticebook.com")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	b, _ := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	fmt.Printf("%s", b)
 }

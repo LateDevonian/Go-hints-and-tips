1f4028511fd7872384c07fbcee288835c026b81e
LateDevonian <agean27@gmail.com>
2019-12-11 19:12:29 +1100

    fixed benchmark

--- chapter5_debug_test/29_benchmarking/benchmark_template_test.go
+++ chapter5_debug_test/29_benchmarking/benchmark_template_test.go
@@ -6,6 +6,8 @@ import (
 	"text/template"
 )
 
+//run with go test -bench .
+
 //want to know what path is faster
 //this zeros in on one average time it takes to compile and run a text template
 func BenchmarkTemplates(b *testing.B) {
@@ -20,7 +22,24 @@ func BenchmarkTemplates(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		t, _ := template.New("test").Parse(tpl)
 		//excecute template and clears the buffer
-		t.Excecute(&buf, data)
+		t.Execute(&buf, data)
 		buf.Reset()
 	}
 }
+func BenchmarkCompiledTemplates(b *testing.B) {
+	b.Logf("b.N is %d\n", b.N)
+	tpl := "Hello {{.Name}}"
+	t, _ := template.New("test").Parse(tpl)
+	//moves template comp out of loop
+	data := &map[string]string{
+		"Name": "World",
+	}
+	var buf bytes.Buffer
+	//run the core of the test b.N times
+	for i := 0; i < b.N; i++ {
+		t.Execute(&buf, data)
+		buf.Reset()
+	}
+}
+
+//benchmark shows the comiled templates shavest time off average loop iteration.
